3878e854c1faa439cf85fdb96685e182d2692200
David Ralph <davidralph@Davids-MacBook-Pro.local>
2023-04-17 09:59:51 +0100

    camera etc

--- system/system.py
+++ system/system.py
@@ -1,14 +1,17 @@
 from gpiozero import MotionSensor
-import time
 from datetime import datetime
+import pygame.camera
+import pygame.image
 
-# database
+
+# databases
 import sqlite3
 conn = sqlite3.connect('../database.db')
 c = conn.cursor()
 
 # create table if not exists
 c.execute('''CREATE TABLE IF NOT EXISTS intrusion (id INTEGER PRIMARY KEY AUTOINCREMENT, time text)''')
+conn.commit()
 
 # painful
 def get_time():
@@ -16,13 +19,18 @@ def get_time():
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
 pir = MotionSensor(17)
+pygame.camera.init()
+camlist = pygame.camera.list_cameras()
+cam = pygame.camera.Camera(camlist[0], (640, 480))
+cam.start()
 
 while True:
   pir.wait_for_motion()
-  print("No intruders")
-  time.sleep(0.1)
-  pir.wait_for_no_motion()
   print("Intruder detected")
-  time.sleep(0.1)
   # write intrusion time etc to database
   c.execute("INSERT INTO intrusion VALUES (NULL, ?)", (get_time(),))
+  image = cam.get_image()
+  pygame.image.save(image, "../photos/" + get_time() + ".jpg")
+  conn.commit()
+  pir.wait_for_no_motion()
+  print("No intruders")
