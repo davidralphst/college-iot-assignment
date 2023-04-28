3d2aa4682b74aacc9041cc6dfda5647f160ef4f4
David Ralph <132060436+davidralphst@users.noreply.github.com>
2023-04-28 11:18:27 +0100

    Add mail sending

--- system/system.py
+++ system/system.py
@@ -1,6 +1,7 @@
 from gpiozero import MotionSensor
 from datetime import datetime
 import subprocess
+import requests
 
 # databases
 import sqlite3
@@ -11,11 +12,18 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS intrusion (id INTEGER PRIMARY KEY AUTOINCREMENT, time text)''')
 conn.commit()
 
-# util function
+# util functions
 def get_time():
     now = datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
+def send_mail():
+    return requests.post("https://api.mailgun.net/v3/aa/messages",
+                         auth=("api", "key")
+                         data={"from": "Security System <mailgun@aa>"
+                               "to": ["collegeemail@gmail.com", "mailgun@aa"]
+                               "subject": "New Intrusion"
+                               "text": "Pain"})
 # init devices
 pir = MotionSensor(17)
 
@@ -27,5 +35,6 @@ while True:
   subprocess.call(["fswebcam", "../photos/" + get_time() + ".jpg"])
   c.execute("INSERT INTO intrusion VALUES (NULL, ?)", (get_time(),))
   conn.commit()
+  send_mail()   
   pir.wait_for_no_motion()
   print("No intruders")
