41c9a34bcf5e582c646bb525c00de80df1186c19
David Ralph <132060436+davidralphst@users.noreply.github.com>
2023-04-28 11:15:22 +0100

    Update system.py

--- system/system.py
+++ system/system.py
@@ -1,6 +1,6 @@
 from gpiozero import MotionSensor
 from datetime import datetime
-from cv2 import *
+import subprocess
 
 # databases
 import sqlite3
@@ -11,21 +11,20 @@ c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS intrusion (id INTEGER PRIMARY KEY AUTOINCREMENT, time text)''')
 conn.commit()
 
-# painful
+# util function
 def get_time():
     now = datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
 
+# init devices
 pir = MotionSensor(17)
-cam_port = 0
-cam = VideoCapture(cam_port)
 
+# main loop
 while True:
   pir.wait_for_motion()
   print("Intruder detected")
-  # write intrusion time etc to database
-  result, image = cam.read()
-  imwrite("../photos/" + get_time() + ".jpg", image)
+  # write intrusion time etc to database and photo
+  subprocess.call(["fswebcam", "../photos/" + get_time() + ".jpg"])
   c.execute("INSERT INTO intrusion VALUES (NULL, ?)", (get_time(),))
   conn.commit()
   pir.wait_for_no_motion()
